6d0b35595a1f4385340e9d72ca7fe1a56707612f
gbrggrn <gustberg107@student.kau.se>
2025-04-24 22:10:15 +0200

    MainWindow documentation

--- Csharp2_ControlTower/MainWindow.xaml.cs
+++ Csharp2_ControlTower/MainWindow.xaml.cs
@@ -26,6 +26,13 @@ namespace Csharp2_ControlTower
         private const int minFlightTime = 5;
         private const int maxChars = 20;
 
+        /// <summary>
+        /// Constructor initializes the inputValues Dictionary.
+        /// Instantiates a new ControlTower.
+        /// Sets the datacontext for this window.
+        /// Sets itemsSource for ListViews.
+        /// Calls AddTestValues in controlTower.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -37,6 +44,9 @@ namespace Csharp2_ControlTower
             controlTower.AddTestValues();
         }
 
+        /// <summary>
+        /// Initializes the inputValues Dictionary with textbox-getters and a description string for each field.
+        /// </summary>
         internal void SetupInputValues()
         {
             inputValues = new Dictionary<Func<string>, string>()
@@ -48,28 +58,41 @@ namespace Csharp2_ControlTower
             };
         }
 
+        /// <summary>
+        /// Validates the input entered and prompts error messages if invalid.
+        /// If valid: creates an airplaneDTO and calls AddPlane from controlTower passing the DTO.
+        /// Calls resetting of input fields.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
             List<string> errorMessages = [];
             int flightTime = 0;
             
+            //Iterates over inputs
             foreach (var pair in inputValues!)
             {
                 string value = pair.Key();
                 string label = pair.Value;
 
+                //If textbox value is null/whitespace
                 if (string.IsNullOrWhiteSpace(value))
                 {
                     errorMessages.Add($"{label} can not be empty");
                 }
+                //If input is too long
                 else if (value.Length > maxChars)
                 {
                     errorMessages.Add($"{label} can not be longer than {maxChars} characters");
                 }
+                //When "Flight time" textbox is reached
                 else if (label == "Flight time")
                 {
+                    //If flight time input can be parsed as int
                     if (int.TryParse(value, out int result))
                     {
+                        //If flighttime input is within limits
                         if (result < minFlightTime || result > maxFlightTime)
                         {
                             errorMessages.Add($"{label} has to be an integer between {minFlightTime} and {maxFlightTime}");
@@ -86,12 +109,14 @@ namespace Csharp2_ControlTower
                 }
             }
 
+            //Check if any errors occured
             if (errorMessages.Count > 0)
             {
                 string result = string.Join("\n", errorMessages);
 
                 MessageBoxes.DisplayErrorBox(result);
             }
+            //Else continue with adding plane to collection
             else
             {
                 var inputs = new AirplaneDTO
@@ -107,6 +132,9 @@ namespace Csharp2_ControlTower
             }
         }
 
+        /// <summary>
+        /// Resets the input fields.
+        /// </summary>
         private void ResetInputFields()
         {
             nameTxtBox.Text = string.Empty;
@@ -115,6 +143,12 @@ namespace Csharp2_ControlTower
             flightTimeTxtBox.Text = string.Empty;
         }
 
+        /// <summary>
+        /// If airplane is selected: calls take off on it.
+        /// Else prompts error message.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TakeOffBtn_Click(object sender, RoutedEventArgs e)
         {
             if (airplaneListView.SelectedIndex != -1)
@@ -128,6 +162,12 @@ namespace Csharp2_ControlTower
             }
         }
 
+        /// <summary>
+        /// If there are airplanes flying: pauses all timers, opens flightlevelwindow.
+        /// When flightlevelWindow is closed, resumes all timers.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ReqAltBtn_Click(object sender, RoutedEventArgs e)
         {
             bool anyCruisingFlights = false;
